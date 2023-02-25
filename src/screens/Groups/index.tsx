e6d7550584ae8196b3414602b1309429c47217b0
frmatias <vandembergneto@gmail.com>
2023-02-25 11:42:16 -0300

    adding theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Text } from "./styles";
+
 
 export function Groups() {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Group</Text>
      
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
