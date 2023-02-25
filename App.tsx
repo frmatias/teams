68aa0e5bc60a71d13538ac5dcc3810fc2fbab006
frmatias <vandembergneto@gmail.com>
2023-02-25 12:15:10 -0300

    loading fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
+
 import theme from '@theme/index';
 
+import { ActivityIndicator } from 'react-native';
+
+import { 
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import { Groups } from '@screens/Groups';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ 
+    Roboto_400Regular, 
+    Roboto_700Bold 
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> :  <ActivityIndicator />}
     </ThemeProvider>
   );
 }
