a8a75c0cb7a1653209de46919a320e67a2b32394
frmatias <vandembergneto@gmail.com>
2023-02-26 14:31:09 -0300

    adding highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
 
 export function Groups() {
   return (
     <Container>
       <Header showBackButton/>
+      <HighLight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   );
 }
