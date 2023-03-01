1af5b0d078e4e780a53d63a7c93fd9103a97109c
frmatias <vandembergneto@gmail.com>
2023-02-28 22:44:32 -0300

    adding flatlist for teams

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
 import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
+
+  const [ groups, setGroups ] = useState(['Amigos', 'Trabalho', 'Esporte']);
+
+
   return (
     <Container>
-      <Header showBackButton/>
+      <Header />
       <HighLight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do ignite"/>
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => <GroupCard title={item} />}
+      />
+
     </Container>
   );
 }
