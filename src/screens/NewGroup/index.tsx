4f4c9cfd2f686bf44c404657d1b2186801adf7ab
frmatias <vandembergneto@gmail.com>
2023-03-04 13:57:20 -0300

    styling input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
+import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup(){
@@ -12,6 +13,9 @@ export function NewGroup(){
                 <HighLight 
                     title="Nova Turma" 
                     subtitle="crie a turma para adicionar as pessoas"
+                />
+                <Input 
+                    placeholder="Nome da turma"
                 />
                 <Button title="Criar" />
             </Content>
